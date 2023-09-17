177712ad6aaddccb1fe9ab6c687883ae68d8c4f8
YogiThiflaini <140322684+YogiThiflaini@users.noreply.github.com>
2023-09-17 13:14:29 +0700

    15-09-23

---
+++ javascript-kelas01-09-23/javacript.js
@@ -0,0 +1,135 @@
+// console.log(2+3);
+// let a= 2;
+// let b= 3;
+// let c= a+b;
+// console.log(c);
+// 3+2*4/2*3*3+2-5
+
+let a= 2;
+let b= 3;
+let c= 4;
+let d= 5;
+let e=((b+a)*c)/(a*b*b)+(a-d);
+console.log(e);
+
+let x=6;
+let y=5;
+// let hasil=x>y;
+if(x>y){
+    console.log("Benar");
+}else{
+    console.log("Salah");
+}
+
+let kkn=80;
+let nilai=70;
+if(nilai>kkn){
+    console.log("Anda Lulus");
+}else{
+    console.log("Tidak Lulus");
+}
+
+
+let bulan=6;
+let tanggal=20;
+let zodiak="Belum dibuat";
+if(bulan==1){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Aquarius";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Taurus";
+    }
+}
+if(bulan==2){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Sagitarius";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Pisces";
+    }
+}
+if(bulan==3){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Aries";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Libra";
+    }
+}
+if(bulan==4){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Gemini";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Cancer";
+    }
+}
+if(bulan==5){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Leo";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Virgo";
+    }
+}
+if(bulan==6){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Scorpio";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Capricorn";
+    }
+}
+if(bulan==7){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Aries";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Taurus";
+    }
+}
+if(bulan==8){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Gemini";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Cancer";
+    }
+}
+if(bulan==9){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Leo";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Virgo";
+    }
+}
+if(bulan==10){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Libra";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Scorpio";
+    }
+}
+if(bulan==11){
+    if(tanggal>0 && tanggal<20){
+        zodiak="Sagitarius";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Capricorn";
+    }
+}
+if(bulan==12){
+    if(tanggal>0 && taggal<20){
+        zodiak="Aquarius";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Pisces";
+    }
+}
+
+    console.log(zodiak);
+
+
